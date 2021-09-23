0b3e551d03938c3538d5c9f48681d054479d0703
arshu-logan <37290915+arshajithwolverine@users.noreply.github.com>
2021-09-24 04:19:18 +0530

    prediction api + result showing implemented

--- functions/index.js
+++ functions/index.js
@@ -7,3 +7,29 @@ var serviceAccount = require("./serviceAccount.json");
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
+
+const express = require('express');
+const cors = require('cors');
+
+let app = express();
+app.use(cors({ origin: true }));
+
+const axios = require('axios');
+const { response } = require("express");
+
+app.post('/Predict', async (req, res) => {
+    data = req.body;
+    console.log(data.text);
+    axios.post(`http://127.0.0.1:8000/predict`, data).then(response => {
+        console.log(response.data);
+        res.json(response.data);
+    }).catch(err => {
+        console.log(err.code)
+        res.json(false);
+    })
+
+})
+
+
+
+exports.Sentiment = functions.https.onRequest(app);
